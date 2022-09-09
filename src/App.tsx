eb7cc1444616688cac4acd9d6c08ec5840335dc2
Chiku <cbhatt@udel.edu>
2022-09-09 19:31:55 -0400

    Completed html-css basics task

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -12,6 +13,50 @@ function App(): JSX.Element {
                 automatically reload.
             </p>
             <p>Hello World.</p>
+            <h1>Task 3</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <img
+                            src="https://www.sherdog.com/image_crop/200/300/_images/fighter/20220331075843_Khamzat_Chimaev_ff.JPG"
+                            alt="Khamzat Chimaev, a UFC fighter"
+                        />
+                        <p>Khamzat Chimaev, a UFC fighter</p>
+                        <div
+                            style={{
+                                border: "5px solid purple",
+                                height: "100px",
+                                width: "500px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <p>
+                            Khamzat Chimaev Facts:
+                            <ol>
+                                <li>Welterweight UFC fighter</li>
+                                <li>Undefeated at both 170 and 185 lbs </li>
+                                <li>
+                                    Missed weight by 7.5 lbs for his upcoming
+                                    bout
+                                </li>
+                            </ol>
+                        </p>
+                        <div
+                            style={{
+                                border: "5px solid purple",
+                                height: "100px",
+                                width: "500px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
